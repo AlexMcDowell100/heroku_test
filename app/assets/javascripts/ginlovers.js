7b8b5980bd2aefb45322b713245aaf5420e205d7
AlexMcDowell100 <amcdowell100@hotmail.com>
2018-07-02 14:27:14 +0100

    Added loading page

--- app/assets/javascripts/ginlovers.js
+++ app/assets/javascripts/ginlovers.js
@@ -51,5 +51,16 @@ function pauseVid() {
     vid.pause(); 
 }
 
-
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
 
